b24ff071fab023e306afe752a033eca21bd3dd1c
Ruben <suarezjruben@gmail.com>
2021-12-21 18:34:05 -0600

    completed Storage Upload/Download section

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # FirstProject
+
+# Installing firebase tools globally
+# $ npm install firebase-tools -g
+
+# Initialize a firebse project 
+# $ firebase init hosting
+
+# Start application localy
+# $ firebase serve
+
+# Start application on server (online)
+# $ firebse deploy
+
--- public/app.js
+++ public/app.js
@@ -16,12 +16,31 @@ document.addEventListener("DOMContentLoaded", event =>{
 
     const myStore = db.collection('Store');
     myStore.onSnapshot(items => {
+        store.innerHTML = "";
         items.forEach(item => {
             data = item.data();
-            var h3 = document.createElement("h3");
-            h3.innerHTML=data.name;
-            store.appendChild(h3)
-            console.log(data)
+            var div1 = document.createElement("div");
+            div1.style.display = "grid";
+            div1.style.gridTemplateColumns = "auto auto auto auto";
+            var name = document.createElement("h4");
+            name.innerHTML=data.name;
+            var count = document.createElement("p");
+            count.innerHTML=data.count;
+            var buy = document.createElement("button");
+            buy.textContent = "buy";
+            buy.id = data.name;
+            buy.value = -1;
+            buy.addEventListener('click', updateCount, false);
+            var retrn = document.createElement("button");
+            retrn.textContent = "return";
+            retrn.id = data.name;
+            retrn.value = 1;
+            retrn.addEventListener('click', updateCount, false);
+            store.appendChild(div1)
+            div1.appendChild(name)
+            div1.appendChild(count)
+            div1.appendChild(buy)
+            div1.appendChild(retrn)
         })
 
     })
@@ -35,6 +54,16 @@ function updatePost(e) {
     myPost.update({count: increment})
 }
 
+function updateCount(e) {
+    console.log(e.target.id);
+    item_name = e.target.id;
+    value = e.target.value;
+    const db = firebase.firestore();
+    const item = db.collection('Store').doc(item_name);
+    item.update({count: firebase.firestore.FieldValue.increment(value)});
+
+}
+
 function googleLogin(){
     const provider = new firebase.auth.GoogleAuthProvider();
 
@@ -45,4 +74,18 @@ function googleLogin(){
         console.log(user);
     })
     .catch(console.log);
+}
+
+function uploadFile(files){
+    const myStorage = firebase.storage().ref()
+    const file = files.item(0)
+    const img_name = file.name
+    console.log(img_name)
+    const imageRef = myStorage.child("images/" + img_name)
+    imageRef.put(file)
+    imageRef.getDownloadURL()
+    .then(url => {
+        console.log(url)
+        document.querySelector('#impUpload').setAttribute('src', url)
+    })
 }
